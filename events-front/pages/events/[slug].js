bb64be00147e33d894a0229a591c0de89c04881d
josephkem <josephkemoy@gmail.com>
2021-10-02 13:52:42 -0400

    Added jsx to create edit and delete buttons along with some styling

--- events-front/pages/events/[slug].js
+++ events-front/pages/events/[slug].js
@@ -1,10 +1,28 @@
+import Link from "next/link";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import Image from "next/image";
 import Layout from "../../components/Layout";
+import styles from "@/styles/Event.module.css";
 import { API_URL } from "@/config/index";
 
 export default function EventPage({ evt }) {
+  const deleteEvent = () => {
+    console.log("delete");
+  };
   return (
     <Layout>
-      <h1>{evt.name}</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a href="">
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+      </div>
     </Layout>
   );
 }
