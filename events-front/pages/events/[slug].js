b2461c9c5359b843881fd45ae8e4e993ae9a7af0
josephkem <josephkemoy@gmail.com>
2021-10-02 19:19:50 -0400

    Made changes to urls to pull in data from strapi

--- events-front/pages/events/[slug].js
+++ events-front/pages/events/[slug].js
@@ -28,7 +28,11 @@ export default function EventPage({ evt }) {
         <h1>{evt.name}</h1>
         {evt.image && (
           <div className={styles.image}>
-            <Image src={evt.image} width={960} height={600} />
+            <Image
+              src={evt.image.formats.medium.url}
+              width={960}
+              height={600}
+            />
           </div>
         )}
 
@@ -48,7 +52,7 @@ export default function EventPage({ evt }) {
 }
 
 export async function getServerSideProps({ query: { slug } }) {
-  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const res = await fetch(`${API_URL}/events?slug=${slug}`);
   const events = await res.json();
 
   return {
