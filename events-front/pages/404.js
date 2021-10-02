00e8decf420f028d09cdabd76a5eb88d3023294f
josephkem <josephkemoy@gmail.com>
2021-10-01 20:46:45 -0400

    Added icon for 404 page

--- events-front/pages/404.js
+++ events-front/pages/404.js
@@ -1,3 +1,4 @@
+import { FaExclamationTriangle } from "react-icons/fa";
 import Layout from "../components/Layout";
 import styles from "../styles/404.module.css";
 import Link from "next/link";
@@ -6,7 +7,9 @@ export default function NotFoundPage() {
   return (
     <Layout title="Page Not Found">
       <div className={styles.error}>
-        <h1>404</h1>
+        <h1>
+          <FaExclamationTriangle /> 404
+        </h1>
         <h4>Sorry, there is nothing here</h4>
         <Link href="/">Go Back Home</Link>
       </div>
