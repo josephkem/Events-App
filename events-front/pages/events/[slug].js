3508c92e94b2e1a83767919b5958e9c36f1ac56d
josephkem <josephkemoy@gmail.com>
2021-10-02 14:02:17 -0400

    Added jsx to diplay data about a single event

--- events-front/pages/events/[slug].js
+++ events-front/pages/events/[slug].js
@@ -22,6 +22,26 @@ export default function EventPage({ evt }) {
             <FaTimes /> Delete Event
           </a>
         </div>
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+
+        <h3>Performers: </h3>
+        <p>{evt.performers}</p>
+        <h3>Description</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href="/events">
+          <a className={styles.back}>{"<"} Go Back</a>
+        </Link>
       </div>
     </Layout>
   );
