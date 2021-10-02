afabbfc284f4f668e7eb1dba66639e3213f26154
josephkem <josephkemoy@gmail.com>
2021-10-02 19:24:18 -0400

    Changed the format display for the date in 2 components

--- events-front/pages/events/[slug].js
+++ events-front/pages/events/[slug].js
@@ -23,7 +23,7 @@ export default function EventPage({ evt }) {
           </a>
         </div>
         <span>
-          {evt.date} at {evt.time}
+          {new Date(evt.date).toLocaleDateString("en-US")} at {evt.time}
         </span>
         <h1>{evt.name}</h1>
         {evt.image && (
