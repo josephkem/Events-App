c54045e124b69cbeba5579994d460018057870e7
josephkem <josephkemoy@gmail.com>
2021-10-03 19:45:12 -0400

    hooked up the authcontext to the Header component and the login and register pages

--- events-front/components/Header.js
+++ events-front/components/Header.js
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import Link from "next/link";
 import Search from "./Search";
+import AuthContext from "@/context/AuthContext";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import styles from "../styles/Header.module.css";
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -19,19 +23,33 @@ export default function Header() {
               <a>Events</a>
             </Link>
           </li>
-          <li>
-            <Link href="/events/add">
-              <a>Add Event</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/account/login">
-              <a className="btn-secondary btn-icon">
-                {" "}
-                <FaSignInAlt /> Login
-              </a>
-            </Link>
-          </li>
+          {user ? (
+            <>
+              <li>
+                <Link href="/events/add">
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/account/dashboard">
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              {" "}
+              <li>
+                <Link href="/account/login">
+                  <a className="btn-secondary btn-icon">
+                    {" "}
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
+
           <li>
             <Link href="/account/register">
               <a className="btn-secondary btn-icon">Register</a>
--- events-front/pages/account/register.js
+++ events-front/pages/account/register.js
@@ -2,6 +2,7 @@ import { FaUser } from "react-icons/fa";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useState, useEffect, useContext } from "react";
+import AuthContext from "@/context/AuthContext";
 import Link from "next/link";
 import Layout from "@/components/Layout";
 import styles from "@/styles//Auth.module.css";
@@ -12,12 +13,16 @@ export default function RegisterPage() {
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [username, setUsername] = useState("");
 
+  const { register, error } = useContext(AuthContext);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (password !== passwordConfirm) {
       toast.error("Passwords do not match");
       return;
     }
+
+    register({ username, email, password });
   };
 
   return (
