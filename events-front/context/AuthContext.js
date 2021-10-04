ad91dced7dece72f9e67121b52b057af7a872766
josephkem <josephkemoy@gmail.com>
2021-10-04 19:55:03 -0400

    Modified logout function in the auth context to fire on the logout api route and redirect the user back to the home page

--- events-front/context/AuthContext.js
+++ events-front/context/AuthContext.js
@@ -43,8 +43,15 @@ export const AuthProvider = ({ children }) => {
   };
 
   //Logout user
-  const logout = async (user) => {
-    console.log("Logout");
+  const logout = async () => {
+    const res = await fetch(`${NEXT_URL}/api/logout`, {
+      method: "POST",
+    });
+
+    if (res.ok) {
+      setUser(null);
+      router.push("/");
+    }
   };
 
   //Check if user is logged in
