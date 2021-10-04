2acd0d2375c72c54ced83579df3314a9099b52d3
josephkem <josephkemoy@gmail.com>
2021-10-04 19:23:48 -0400

    Created a page for the user dashboard and updated the checkuser function in the auth context to fire on the user api route

--- events-front/context/AuthContext.js
+++ events-front/context/AuthContext.js
@@ -8,6 +8,10 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
+  const router = useRouter();
+
+  useEffect(() => checkUserLoggedIn(), []);
+
   //Register User
   const register = async (user) => {
     console.log(user);
@@ -45,7 +49,15 @@ export const AuthProvider = ({ children }) => {
 
   //Check if user is logged in
   const checkUserLoggedIn = async (user) => {
-    console.log(user);
+    const res = await fetch(`${NEXT_URL}/api/user`);
+    const data = await res.json();
+
+    if (res.ok) {
+      setUser(data.user);
+      router.push("/account/dashboard");
+    } else {
+      setUser(null);
+    }
   };
 
   return (
