c1fa129b1eac81522b7979228ec2080ecf90fa7c
josephkem <josephkemoy@gmail.com>
2021-10-05 15:22:26 -0400

    Created component to build the map for the events

--- events-front/pages/events/[slug].js
+++ events-front/pages/events/[slug].js
@@ -5,6 +5,7 @@ import { FaPencilAlt, FaTimes } from "react-icons/fa";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import Layout from "../../components/Layout";
+import EventMap from "../../components/EventMap";
 import styles from "@/styles/Event.module.css";
 import { API_URL } from "@/config/index";
 
@@ -33,6 +34,8 @@ export default function EventPage({ evt }) {
         <p>{evt.description}</p>
         <h3>Venue: {evt.venue}</h3>
         <p>{evt.address}</p>
+
+        <EventMap evt={evt} />
 
         <Link href="/events">
           <a className={styles.back}>{"<"} Go Back</a>
