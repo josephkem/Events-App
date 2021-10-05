95d4a684fc09972242a01e01177b04707ab0f433
josephkem <josephkemoy@gmail.com>
2021-10-05 14:25:25 -0400

    Added token to the edit page to authorize image uploads

--- events-front/pages/events/edit/[id].js
+++ events-front/pages/events/edit/[id].js
@@ -167,7 +167,11 @@ export default function EditEventPage({ evt, token }) {
         </button>
       </div>
       <Modal show={showModal} onClose={() => setShowModal(false)}>
-        <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
+        <ImageUpload
+          evtId={evt.id}
+          imageUploaded={imageUploaded}
+          token={token}
+        />
       </Modal>
     </Layout>
   );
