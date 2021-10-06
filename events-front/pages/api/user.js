dee00985026004d12d8e70abda963b9d9962fb60
josephkem <josephkemoy@gmail.com>
2021-10-05 23:40:05 -0400

    Fixed issue that caused error in the console

--- events-front/pages/api/user.js
+++ events-front/pages/api/user.js
@@ -4,11 +4,11 @@ import { API_URL } from "@/config/index";
 export default async (req, res) => {
   if (req.method === "GET") {
     if (!req.headers.cookie) {
-      res.status(403).json({ mesage: "Not Authorized" });
+      res.status(403).json({ message: "Not Authorized" });
       return;
     }
 
-    const { token } = cookie.parse(req.headers);
+    const { token } = cookie.parse(req.headers.cookie);
 
     const strapiRes = await fetch(`${API_URL}/users/me`, {
       method: "GET",
